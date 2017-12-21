c79a1614c3f5f3101ea091fad66a6d12f8b85bfb
Paul Demorest <pdemores@nrao.edu>
2017-12-21 10:13:18 -0700

    Add example usage script.

---
+++ example/rfgpu_example.py
@@ -0,0 +1,48 @@
+import numpy as np
+from numpy.fft import fftshift
+import rfgpu
+
+# B-config uv points
+uv = np.loadtxt('uv_B.dat')
+
+nbl = uv.shape[0]
+nchan = 32
+ntime = 128
+npix = 1024
+upix = npix
+vpix = npix/2 + 1
+
+# Set up processing classes
+grid = rfgpu.Grid(nbl, nchan, ntime, upix, vpix)
+image = rfgpu.Image(npix,npix)
+
+# Data buffers on GPU
+vis_raw = rfgpu.GPUArrayComplex(nbl*nchan*ntime)
+vis_grid = rfgpu.GPUArrayComplex(upix*vpix)
+img_grid = rfgpu.GPUArrayReal(npix*npix)
+
+# Send uv params
+grid.set_uv(uv[:,0], uv[:,1]) # u, v in us
+grid.set_freq(np.linspace(1000.0,2000.0,nchan)) # freq in MHz
+grid.set_shift(np.zeros(nchan,dtype=int)) # dispersion shift per chan in samples
+
+# Compute gridding transform
+grid.compute()
+
+# Generate some random visibility data
+vis_data = np.array(vis_raw,copy=False)
+vis_data[:] = np.random.randn(vis_raw.len()) \
+        + 1.0j*np.random.randn(vis_raw.len()) \
+        + 0.5  # Point source at phase center
+vis_raw.h2d()  # Send it to GPU memory
+
+# Run gridding on time slice 0 of data array
+grid.operate(vis_raw, vis_grid, 0)
+
+# Do FFT
+image.operate(vis_grid, img_grid)
+
+# Get image back from GPU
+img_grid.d2h()
+img_data = np.array(img_grid,copy=False).reshape((npix,npix))
+img_data = fftshift(img_data) # put center pixel in middle of image
